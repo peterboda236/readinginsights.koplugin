153cf707dc52a67ee30c611055adfe921b4141f2
peterboda236 <130438938+peterboda236@users.noreply.github.com>
2026-07-01 08:33:17 +0200

    V1.3 - Moved to plugin from patch

---
+++ _meta.lua
@@ -0,0 +1,7 @@
+local _ = require("gettext")
+
+return {
+    name = "readinginsights",
+    fullname = _("Reading insights"),
+    description = _([[Full-screen, scrollable reading-history popup built on top of statistics.sqlite3: last-week bar chart, streaks, yearly/monthly stats and charts, and all-time totals. Adds an entry under Statistics > Reading insights.]]),
+}
